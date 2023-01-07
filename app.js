125b9c6b27577634fec0bc6368af4e2753091128
Zeljko <zeljko.cavic@playstudios.com>
2023-01-07 17:46:55 +0100

    GraphQL types

--- app.js
+++ app.js
@@ -5,18 +5,34 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
     "/graphql",
     graphqlHTTP({
         schema: buildSchema(`
+            type Event {
+                _id: ID!
+                title: String!
+                description: String!
+                price: Float!
+                date: String!
+            }
+
+            input EventInput {
+                title: String!
+                description: String!
+                price: Float!
+            }
+
             type RootQuery {
-                events: [String!]!
+                events: [Event!]!
             }
 
             type RootMutation {
-                createEvent(name: String!): String!
+                createEvent(eventInput: EventInput!): Event!
             }
 
             schema {
@@ -26,11 +42,18 @@ app.use(
         `),
         rootValue: {
             events: () => {
-                return ["Exit", "Sensation", "MetalCamp"];
+                return events;
             },
             createEvent: (arg) => {
-                const eventName = arg.name;
-                return eventName;
+                const event = {
+                    _id: Math.random().toString(),
+                    title: arg.eventInput.title,
+                    description: arg.eventInput.description,
+                    price: +arg.eventInput.price,
+                    date: new Date().toISOString()
+                };
+                events.push(event);
+                return event;
             }
         },
         graphiql: true
