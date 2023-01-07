90adb430cec5acdc850748bb02a164b1db6d4df6
Zeljko <zeljko.cavic@playstudios.com>
2023-01-07 15:32:36 +0100

    GraphQL API

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-    return res.status(200).json({ status: "Success", data: {} });
-});
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                events: [String!]!
+            }
+
+            type RootMutation {
+                createEvent(name: String!): String!
+            }
+
+            schema {
+                query: RootQuery,
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return ["Exit", "Sensation", "MetalCamp"];
+            },
+            createEvent: (arg) => {
+                const eventName = arg.name;
+                return eventName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(3000);
