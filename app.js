9ea9965c86d8ab2636a92d739575edda84f65c2e
Zeljko <zeljko.cavic@playstudios.com>
2023-01-08 15:51:37 +0100

    Dynamic relations

--- app.js
+++ app.js
@@ -4,10 +4,24 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const mongoose = require("mongoose");
 const Event = require("./models/event");
+const User = require("./models/user");
+const bcryptjs = require("bcryptjs");
 
 const app = express();
 
-const events = [];
+const getUser = async (userId) => {
+    const userDb = await User.findById(userId);
+    const user = {
+        ...userDb._doc,
+        createdEvents: getEvents(userDb._doc.createdEvents)
+    };
+    return user;
+};
+
+const getEvents = async (eventIds) => {
+    const events = await Event.find({ id: { $in: eventIds } });
+    return events.map((event) => ({ ...event._doc, creator: getUser(event._doc.creator) }));
+};
 
 app.use(bodyParser.json());
 
@@ -21,6 +35,7 @@ app.use(
                 description: String!
                 price: Float!
                 date: String!
+                creator: User!
             }
 
             input EventInput {
@@ -29,12 +44,24 @@ app.use(
                 price: Float!
             }
 
+            type User {
+                _id: ID!
+                email: String!
+                createdEvents: [Event!]
+            }
+
+            input UserInput {
+                email: String!
+                password: String!
+            }
+
             type RootQuery {
                 events: [Event!]!
             }
 
             type RootMutation {
                 createEvent(eventInput: EventInput!): Event!
+                createUser(userInput: UserInput!): User!
             }
 
             schema {
@@ -44,11 +71,24 @@ app.use(
         `),
         rootValue: {
             events: async () => {
-                return await Event.find();
+                const events = await Event.find();
+                return events.map((event) => ({ ...event._doc, creator: getUser(event._doc.creator) }));
             },
             createEvent: async (arg) => {
-                const event = new Event({ ...arg.eventInput, date: new Date() });
+                const user = await User.findById("63b9e0ea47ec9e3ef552abc1");
+                if (!user) throw new Error("User not exist!");
+                const event = new Event({ ...arg.eventInput, date: new Date(), creator: "63b9e0ea47ec9e3ef552abc1" });
+                user.createdEvents.push(event);
+                await user.save();
                 return await event.save();
+            },
+            createUser: async (arg) => {
+                if (await User.findOne({ email: arg.userInput.email })) throw new Error("User exist!");
+                const user = new User({
+                    email: arg.userInput.email,
+                    password: await bcryptjs.hash(arg.userInput.password, 12)
+                });
+                return await user.save();
             }
         },
         graphiql: true
